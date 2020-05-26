b2722bbfde4a82623987003a965b1547647e6f99
Mr. Radd <conrad.horton@gmail.com>
2020-05-26 14:00:58 -0700

    Floop doop

--- Class1.cs
+++ Class1.cs
@@ -11,11 +11,6 @@ public class Class1
 		Console.WriteLine("This is from foo.");
 	}
 
-	public void bar()
-	{
-		Console.WriteLine("Hello there!");
-	}
-	
 	public string myName()
 	{
 		return "My name is Herp Derp.";
